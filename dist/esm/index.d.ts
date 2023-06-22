21aa960177459b8067db1ed21006db0d3863d0ce
Darren Gates <darren.gates@wearehathway.com>
2023-06-22 13:32:25 -0700

    update

--- dist/esm/index.d.ts
+++
@@ -1,4 +0,0 @@
-import type { BrazeCapacitorPlugin } from './definitions';
-declare const BrazeCapacitor: BrazeCapacitorPlugin;
-export * from './definitions';
-export { BrazeCapacitor };
--- dist/esm/index.js
+++
@@ -1,7 +0,0 @@
-import { registerPlugin } from '@capacitor/core';
-const BrazeCapacitor = registerPlugin('BrazeCapacitor', {
-    web: () => import('./web').then(m => new m.BrazeCapacitorWeb()),
-});
-export * from './definitions';
-export { BrazeCapacitor };
-//# sourceMappingURL=index.js.map
